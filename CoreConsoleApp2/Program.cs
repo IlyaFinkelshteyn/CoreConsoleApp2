100d40ab6aa12c3663f30e0a79794b2a528a7f50
ilyaf f <ilyaf@appveyor.com>
2018-04-30 19:02:47 -0700

    first commit

---
+++ CoreConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
